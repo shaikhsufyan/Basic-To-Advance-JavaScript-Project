54f3174849a7c47bc394527e26d1d71fa4bb3d2e
shaikh sufyan <shaikhsufyan1811@gmail.com>
2023-04-21 15:28:56 +0530

    Image Slider Using Javascript

---
+++ ImageSlider/index.js
@@ -0,0 +1,31 @@
+let slides = document.querySelectorAll(".slide");
+
+let count = 0;
+
+slides.forEach(function(slides, index){
+  slides.style.left=`${index * 100}%`;
+});
+
+function next(){
+  count++;
+  if(count == slides.length){
+     count=0;
+  }
+  myFun();
+
+}
+
+function prev(){
+  count--;
+  if(count == -1){
+     count = slides.length-1;
+  }
+  myFun();
+
+}
+
+function  myFun(){
+ slides.forEach(function(slides){
+ slides.style.transform=`translateX(-${count * 100}%)`
+ })
+}
