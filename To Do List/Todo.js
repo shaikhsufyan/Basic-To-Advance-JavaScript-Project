f1bfae317143728f6e6baade7a956cf5c252ed2f
shaikh sufyan <shaikhsufyan1811@gmail.com>
2023-05-27 10:55:36 +0530

    TO DO App

---
+++ To Do List/Todo.js
@@ -0,0 +1,17 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("Please Enter Task")
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        inputs.value="";
+        newEle.querySelector("i").addEventListener("click" , remove);
+        function remove(){
+            newEle.remove()
+        }
+    }
+}
