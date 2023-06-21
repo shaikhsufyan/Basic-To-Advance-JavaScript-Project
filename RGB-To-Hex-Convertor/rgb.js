8f0e7bef04ec43b2b4fa34e000c247941cb1d2f0
shaikh sufyan <shaikhsufyan1811@gmail.com>
2023-06-21 17:12:39 +0530

    Converting RGB Color Code To HEX Color Code Using JavaScript

---
+++ RGB-To-Hex-Convertor/rgb.js
@@ -0,0 +1,25 @@
+let r = document.getElementById("red");
+let g = document.getElementById("green");
+let b = document.getElementById("blue");
+
+function myFun() {
+
+    r = convertor(r.value);
+    g = convertor(g.value);
+    b = convertor(b.value);
+
+    let add = "#" + r + g + b;
+    let upper = add.toUpperCase()
+    document.getElementById("hexValue").value = upper
+    document.body.style.backgroundColor = upper;
+    console.log(upper);
+
+    document.getElementById("red").value="";
+    document.getElementById("green").value="";
+    document.getElementById("blue").value="";
+
+    function convertor(rgb) {
+        let get = Number(rgb).toString(16);
+        return get;
+    }
+}
