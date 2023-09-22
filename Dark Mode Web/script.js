8f14c8c04cf78e87dc957fab6becde9c245735a5
shaikh sufyan <shaikhsufyan1811@gmail.com>
2023-09-22 09:22:14 +0530

    Convert Web into Light Mode and Dark Mode

---
+++ Dark Mode Web/script.js
@@ -0,0 +1,12 @@
+let icon = document.getElementById("icon");
+
+ 
+icon.addEventListener("click", function(){
+    document.querySelector("body").classList.toggle("active");
+    if(document.querySelector("body").className == "active"){
+    icon.className="fa-solid fa-sun";
+    }else{
+    icon.className="fa-solid fa-moon";
+
+    }
+})
